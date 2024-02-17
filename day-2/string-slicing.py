ad9c1243d065eeff3cea3c1a83caf28369ef0f6f
Vlad Hurin <longsno@gmail.com>
2024-02-18 00:13:59 +0100

    l: Day 2 of py learning

---
+++ day-2/string-slicing.py
@@ -0,0 +1,14 @@
+name = "Ula Dick"
+
+first_name = name[:3]
+last_name = name[4:]
+funky_name = name[0:8:1]
+reverse = name[::-1]
+
+website_a = "https://google.com"
+website_b = "https://mopme.com"
+
+sliced = slice(8, -4)
+
+print(website_a[sliced])
+print(website_b[sliced])
--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@ Day 1:
 1) Variables
 2) String methods
 3) Type Cast
+
+Day 2: 
+1) User input
+2) Math functions
+3) string slicing
