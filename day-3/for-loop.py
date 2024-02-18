d3e7b4a8a1c37b92e514d4cacb0408a6673a05c5
Vlad Hurin <longsno@gmail.com>
2024-02-18 21:14:44 +0100

    l: Day 3 of py learning

---
+++ day-3/for-loop.py
@@ -0,0 +1,17 @@
+# for i in range(10):
+#     print(i)
+#
+# for i in range(5, 55 + 1):
+#     print(i)
+#
+# for i in range(2, 100 + 1, 2):
+#     print(i)
+#
+# for i in "Ula Dick":
+#     print(i)
+import time
+
+for i in range (10, 0, -1):
+    print(i)
+    time.sleep(1)
+print("HPNY")
--- readme.md
+++ readme.md
@@ -9,3 +9,10 @@ Day 2:
 1) User input
 2) Math functions
 3) string slicing
+
+Day 3:
+1) if statement
+2) logical operators
+3) while loop
+4) for loop
+5) nested loops
